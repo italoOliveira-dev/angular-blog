a4632d1732baf411df9ec814fee5cbd488fc4a50
Italo Oliveira <italooliveira-dev@hotmail.com>
2023-12-17 22:24:54 -0300

    feat: components creted with style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [MenuTitleComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent]
 })
 export class HomeComponent {
 
